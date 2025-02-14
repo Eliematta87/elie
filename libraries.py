2266dd8aa9d21c06c57dee8cb76bf65984e1789f
Eliematta87 <eliematta9@gmail.com>
2025-02-14 08:26:15 +0200

    Update and rename my_socket.py to libraries.py

---
+++ libraries.py
@@ -0,0 +1,12 @@
+# INPUT
+import os
+import sys
+import socket
+
+# Using built-in modules for system operations and networking
+print(f"Current directory: {os.getcwd()}")
+print(f"Python version: {sys.version}")
+print(f"Hostname: {socket.gethostname()}")
+
+# OUTPUT
+# RUN THE SCRIPT ==> U WILL SEE!!!
